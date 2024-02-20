5d56579acf45c93aa7e98fc74f976c5f3553b828
MarkPrys <mark.prischepa@gmail.com>
2024-02-20 16:55:40 +0100

    task 2 is done

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,2 +1,52 @@
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
+
+
+    function handleSubmit(event) {
+      event.preventDefault();
+
+      const { delay, state } = this.elements;
+      const delayValue = delay.value;
+      const stateValue = state.value;
+
+      delay.value = '';
+      state.value = null;
+
+      const radioButtons = document.querySelectorAll('input[name="state"]');
+      radioButtons.forEach(button => button.checked = false);
+
+      const promise = createDelayPromise(delayValue, stateValue);
+
+      promise.then(
+        promiseFulfilled,
+        promiseRejected
+      );
+    }
+
+  
+    function createDelayPromise(delayValue, stateValue) {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          if (stateValue === 'fulfilled') {
+            resolve(delayValue);
+          } else {
+            reject(delayValue);
+          }
+        }, delayValue);
+      });
+    }
+
+    function promiseFulfilled(value) {
+      iziToast.success({
+        message: (`✅ Fulfilled promise in ${value}ms`),
+    });
+    }
+
+    function promiseRejected(value) {
+      iziToast.error({
+        message: (`❌ Rejected promise in ${value}ms`),
+    });
+    }
+
+    const form = document.querySelector('.form');
+    form.addEventListener('submit', handleSubmit);
